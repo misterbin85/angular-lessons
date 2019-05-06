63b39c1921e2e756c011f55e98154af0f4bae200
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-05-07 01:29:11 +0300

    new components

--- angular-lesson-app/src/app/app.module.ts
+++ angular-lesson-app/src/app/app.module.ts
@@ -1,12 +1,27 @@
+import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShopppingListComponent } from './shoppping-list/shoppping-list.component';
+import { ShoppingEditComponent } from './shoppping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
